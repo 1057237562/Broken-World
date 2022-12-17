4fea5db2df3e6c9c0cf5c9b693956389822b47fb
PANJIAN <1057237562@qq.com>
2022-12-17 16:43:30 +0800

    update

--- src/main/java/com/brainsmash/broken_world/Main.java
+++ src/main/java/com/brainsmash/broken_world/Main.java
@@ -1,6 +1,13 @@
 package com.brainsmash.broken_world;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +15,16 @@ public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
+	public static final Block[] blocks = {
+			new FallingBlock(FabricBlockSettings.of(Material.GOURD).strength(1.0f)),
+			new Block(AbstractBlock.Settings.of(Material.STONE).strength(1.0f))
+	};
+	public static final String[] blocknames = {"moon_sand","moon_stone"};
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		for(int i = 0;i<blocks.length;i++){
+			Registry.register(Registry.BLOCK, new Identifier("broken_world", blocknames[i]), blocks[i]);
+			Registry.register(Registry.ITEM, new Identifier("broken_world", blocknames[i]), new BlockItem(blocks[i], new FabricItemSettings()));
+		}
 	}
 }
--- src/main/java/com/brainsmash/broken_world/mixin/MainMixin.java
+++ src/main/java/com/brainsmash/broken_world/mixin/MainMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MainMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		Main.LOGGER.info("This line is printed by an example mod mixin!");
+
 	}
 }
