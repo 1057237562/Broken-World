de1ad320b1c88897e469fd751ca8e33405fb70ab
PANJIAN <1057237562@qq.com>
2022-12-17 16:09:56 +0800

    update

--- src/main/java/com/brainsmash/broken_world/mixin/MainMixin.java
+++ src/main/java/com/brainsmash/broken_world/mixin/MainMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.brainsmash.broken_world.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.brainsmash.broken_world.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class MainMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
