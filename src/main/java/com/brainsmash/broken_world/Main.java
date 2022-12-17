1928e7f359c11a0a4f43ec2a54758a32180f9f50
PANJIAN <1057237562@qq.com>
2022-12-17 19:55:56 +0800

    update

--- src/main/java/com/brainsmash/broken_world/Main.java
+++ src/main/java/com/brainsmash/broken_world/Main.java
@@ -7,26 +7,51 @@ import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Block[] blocks = {
-			new FallingBlock(FabricBlockSettings.of(Material.SOIL).strength(1.0f)),
+			new FallingBlock(AbstractBlock.Settings.of(Material.MOSS_BLOCK).strength(1.0f)),
 			new Block(AbstractBlock.Settings.of(Material.STONE).strength(1.0f)),
 			new Block(AbstractBlock.Settings.of(Material.STONE).strength(1.5f)),
 			new Block(AbstractBlock.Settings.of(Material.STONE).strength(2.0f))
 	};
 	public static final String[] blocknames = {"moon_sand","moon_stone","moon_iron_ore","moon_gold_ore"};
+	private static final String[] configurenames = {"moon_sand","moon_iron_ore","moon_gold_ore"};
+	private static ConfiguredFeature<?, ?>[] configuredFeatures = {
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[0].getDefaultState(),9)),
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[2].getDefaultState(),9)),
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[3].getDefaultState(),9))
+	};
+
+	public static PlacedFeature[] placedFeatures = {
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[0]),Arrays.asList(CountPlacementModifier.of(20),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64)))),
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[1]),Arrays.asList(CountPlacementModifier.of(20),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64)))),
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[2]),Arrays.asList(CountPlacementModifier.of(20),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))))
+	};
 	@Override
 	public void onInitialize() {
 		for(int i = 0;i<blocks.length;i++){
 			Registry.register(Registry.BLOCK, new Identifier("broken_world", blocknames[i]), blocks[i]);
 			Registry.register(Registry.ITEM, new Identifier("broken_world", blocknames[i]), new BlockItem(blocks[i], new FabricItemSettings()));
+		}
+		for(int i = 0;i<configuredFeatures.length;i++){
+			Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,new Identifier("broken_world", configurenames[i]), configuredFeatures[i]);
+			Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier("broken_world", configurenames[i]),placedFeatures[i]);
 		}
 	}
 }
