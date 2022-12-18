1a3184155637a48b8e9e4c781d514c1c7f88adbe
PANJIAN <1057237562@qq.com>
2022-12-18 14:01:59 +0800

    update

--- src/main/java/com/brainsmash/broken_world/Main.java
+++ src/main/java/com/brainsmash/broken_world/Main.java
@@ -3,11 +3,11 @@ package com.brainsmash.broken_world;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.*;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -17,9 +17,8 @@ import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.function.Supplier;
 
@@ -28,38 +27,40 @@ public class Main implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MODID = "broken_world";
-	public static final Block[] blocks = {
-			new FallingBlock(AbstractBlock.Settings.of(Material.MOSS_BLOCK).strength(1.0f)),
-			new Block(AbstractBlock.Settings.of(Material.STONE).strength(1.0f)),
-			new Block(AbstractBlock.Settings.of(Material.STONE).strength(1.5f)),
-			new Block(AbstractBlock.Settings.of(Material.STONE).strength(2.0f))
-	};
-	public static final Item[] items = {
-			new BlockItem(blocks[0],new FabricItemSettings()),
-			new BlockItem(blocks[1],new FabricItemSettings()),
-			new BlockItem(blocks[2],new FabricItemSettings()),
-			new BlockItem(blocks[3],new FabricItemSettings())
-	};
-	public static final String[] blocknames = {"moon_sand","moon_stone","moon_iron_ore","moon_gold_ore"};
-	private static final String[] configurenames = {"moon_sand","moon_iron_ore","moon_gold_ore"};
-	private static ConfiguredFeature<?, ?>[] configuredFeatures = {
-			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[0].getDefaultState(),9)),
-			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[2].getDefaultState(),9)),
-			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[3].getDefaultState(),9))
-	};
-
-	public static PlacedFeature[] placedFeatures = {
-			new PlacedFeature(RegistryEntry.of(configuredFeatures[0]),Arrays.asList(CountPlacementModifier.of(42),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(128)))),
-			new PlacedFeature(RegistryEntry.of(configuredFeatures[1]),Arrays.asList(CountPlacementModifier.of(63),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(128)))),
-			new PlacedFeature(RegistryEntry.of(configuredFeatures[2]),Arrays.asList(CountPlacementModifier.of(36),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(128))))
-	};
-
 	private static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MODID, "itemgroup"), new Supplier<ItemStack>() {
 		@Override
 		public ItemStack get() {
 			return new ItemStack(items[0]);
 		}
 	});
+
+	public static final Block[] blocks = {
+			new FallingBlock(AbstractBlock.Settings.of(Material.AGGREGATE).sounds(BlockSoundGroup.SAND).strength(1.0f)),
+			new Block(FabricBlockSettings.copyOf(Blocks.STONE).strength(2.0f,2.0f)),
+			new OreBlock(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).requiresTool().strength(3.0f,3.0f)),
+			new OreBlock(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).requiresTool().strength(3.0f,3.0f)),
+			new Block(FabricBlockSettings.of(Material.METAL).sounds(BlockSoundGroup.METAL).requiresTool().strength(2.0f,10f))
+	};
+	public static final Item[] items = {
+			new BlockItem(blocks[0],new FabricItemSettings().group(ITEM_GROUP)),
+			new BlockItem(blocks[1],new FabricItemSettings().group(ITEM_GROUP)),
+			new BlockItem(blocks[2],new FabricItemSettings().group(ITEM_GROUP)),
+			new BlockItem(blocks[3],new FabricItemSettings().group(ITEM_GROUP)),
+			new BlockItem(blocks[4],new FabricItemSettings().group(ITEM_GROUP))
+	};
+	public static final String[] blocknames = {"moon_sand","moon_stone","moon_iron_ore","moon_gold_ore","teleporter_frame"};
+	private static final String[] configurenames = {"moon_sand","moon_iron_ore","moon_gold_ore"};
+	private static ConfiguredFeature<?, ?>[] configuredFeatures = {
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[0].getDefaultState(),21)),
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[2].getDefaultState(),9)),
+			new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,blocks[3].getDefaultState(),9))
+	};
+
+	public static PlacedFeature[] placedFeatures = {
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[0]),Arrays.asList(CountPlacementModifier.of(12),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64)))),
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[1]),Arrays.asList(CountPlacementModifier.of(24),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64)))),
+			new PlacedFeature(RegistryEntry.of(configuredFeatures[2]),Arrays.asList(CountPlacementModifier.of(24),SquarePlacementModifier.of(),HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))))
+	};
 
 	@Override
 	public void onInitialize() {
@@ -71,5 +72,6 @@ public class Main implements ModInitializer {
 			Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,new Identifier(MODID, configurenames[i]), configuredFeatures[i]);
 			Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(MODID, configurenames[i]),placedFeatures[i]);
 		}
+		CustomPortalBuilder.beginPortal().frameBlock(blocks[4]).lightWithItem(Items.DIAMOND).destDimID(new Identifier(MODID,"moon")).tintColor(Color.WHITE.getRGB()).registerPortal();
 	}
 }
