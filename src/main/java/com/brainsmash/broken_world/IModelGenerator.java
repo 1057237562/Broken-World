829c9dd75322ca3f131073cbe374b237a97bb8b4
PANJIAN <1057237562@qq.com>
2022-12-22 21:53:39 +0800

    Update

--- src/main/java/com/brainsmash/broken_world/IModelGenerator.java
+++ src/main/java/com/brainsmash/broken_world/IModelGenerator.java
@@ -5,6 +5,9 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Model;
+import net.minecraft.data.client.Models;
+import net.minecraft.item.Item;
 
 public class IModelGenerator extends FabricModelProvider {
     public IModelGenerator(FabricDataGenerator dataGenerator) {
@@ -13,13 +16,15 @@ public class IModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        for(Block block: Main.baseblock) {
+        /*for(Block block: Main.baseblock) {
             blockStateModelGenerator.registerSimpleCubeAll(block);
-        }
+        }*/
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        /*for(Item item: Main.items){
+            itemModelGenerator.register(item, Models.GENERATED);
+        }*/
     }
 }
