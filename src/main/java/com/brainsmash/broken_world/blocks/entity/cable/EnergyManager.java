cbad0cf87658d94b31095ab246154e10d8900d6b
PANJIAN <1057237562@qq.com>
2022-12-24 19:52:34 +0800

    Revert "Revert "Clean""
    This reverts commit 8ac6097c880d5e3f1027edde3a7e1b75a5f1332d.

---
+++ src/main/java/com/brainsmash/broken_world/blocks/entity/cable/EnergyManager.java
@@ -0,0 +1,9 @@
+package com.brainsmash.broken_world.blocks.entity.cable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnergyManager {
+    private static final List<CableBlockEntity> cableList = new ArrayList<>();
+
+}
