3db2a9d7cc3f1840a34fae89cd8d6c743e0c0b2d
fishBone000 <FishBoneEK@outlook.com>
2023-01-03 09:09:01 +0000

    update

---
+++ src/main/java/com/brainsmash/broken_world/client/render/block/entity/CreativeGeneratorBlockEntityRenderer.java
@@ -0,0 +1,6 @@
+package com.brainsmash.broken_world.client.render.block.entity;
+
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+
+public class CreativeGeneratorBlockEntityRenderer extends BlockEntityRenderer<CreativeGeneratorBlockEntity> {
+}
