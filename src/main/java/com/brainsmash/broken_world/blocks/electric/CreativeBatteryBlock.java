99c8f8469f207dfb7cc8321e1872d16e346e9a25
PANJIAN <1057237562@qq.com>
2023-01-03 16:37:28 +0800

    Update

--- src/main/java/com/brainsmash/broken_world/blocks/electric/CreativeBatteryBlock.java
+++ src/main/java/com/brainsmash/broken_world/blocks/electric/CreativeBatteryBlock.java
@@ -20,7 +20,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
-public class CreativeBatteryBlock extends BlockWithEntity {
+public class CreativeBatteryBlock extends BatteryBlock {
 
     public CreativeBatteryBlock(Settings settings) {
         super(settings);
@@ -34,20 +34,7 @@ public class CreativeBatteryBlock extends BlockWithEntity {
 
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        return (world1, pos, state1, blockEntity) -> ((CreativeBatteryBlockEntity) blockEntity).tick(world1, pos, state1, (BatteryBlockEntity) blockEntity);
-    }
-
-    @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        //With inheriting from BlockWithEntity this defaults to INVISIBLE, so we need to change that!
-        return BlockRenderType.MODEL;
-    }
-
-    @Override
-    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
-        if(!world.isClient())
-            EnergyManager.UpdateGraph(world,pos);
-        super.onBroken(world, pos, state);
+        return (world1, pos, state1, blockEntity) -> ((CreativeBatteryBlockEntity) blockEntity).tick(world1, pos, state1, (CreativeBatteryBlockEntity) blockEntity);
     }
 
     @Override
