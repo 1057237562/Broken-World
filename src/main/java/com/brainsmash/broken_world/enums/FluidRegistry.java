08fae4a2a8bd6ceb1dfec1a477ccee74d73407e3
PANJIAN <1057237562@qq.com>
2023-01-04 14:15:00 +0800

    Update

---
+++ src/main/java/com/brainsmash/broken_world/enums/FluidRegistry.java
@@ -0,0 +1,7 @@
+package com.brainsmash.broken_world.enums;
+
+public enum FluidRegistry {
+    OIL,
+    POLLUTED_WATER,
+    ACID
+}
---
+++ src/main/java/com/brainsmash/broken_world/enums/ItemRegistry.java
@@ -0,0 +1,8 @@
+package com.brainsmash.broken_world.enums;
+
+public enum ItemRegistry {
+    TITANIUM_INGOT,
+    TUNGSTEN_INGOT,
+    OXYGEN_GENERATOR_PACK,
+    EMERGENCY_TELEPORTER
+}
