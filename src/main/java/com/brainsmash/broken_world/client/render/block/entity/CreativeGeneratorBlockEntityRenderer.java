7b94286727af3e636ead766b667f57c1941d536f
fishBone000 <FishBoneEK@outlook.com>
2023-01-04 12:19:38 +0000

    Creative Generator now renders smoothly, except for the item

--- src/main/java/com/brainsmash/broken_world/client/render/block/entity/CreativeGeneratorBlockEntityRenderer.java
+++ src/main/java/com/brainsmash/broken_world/client/render/block/entity/CreativeGeneratorBlockEntityRenderer.java
@@ -14,18 +14,24 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3f;
 
 public class CreativeGeneratorBlockEntityRenderer implements BlockEntityRenderer<CreativeGeneratorBlockEntity> {
+    private static final String FRAME = "frame";
     private static final String SHELL = "shell";
     private static final String MID_ORB = "mid_orb";
     private static final String INNER_ORB = "inner_orb";
     private static final Identifier TEXTURE = new Identifier(Main.MODID, "textures/entity/creative_generator.png");
     public static final EntityModelLayer CREATIVE_GENERATOR = new EntityModelLayer(new Identifier(Main.MODID, "creative_generator"), "main");
+    private final ModelPart frame;
     private final ModelPart shell;
     private final ModelPart midOrb;
     private final ModelPart innerOrb;
+    private static final long CYCLE = 48;
 
     public CreativeGeneratorBlockEntityRenderer(BlockEntityRendererFactory.Context ctx){
         ModelPart modelPart = ctx.getLayerModelPart(CREATIVE_GENERATOR);
+        frame = modelPart.getChild(FRAME);
+        frame.scale(new Vec3f(-0.01f, -0.01f, -0.01f));
         shell = modelPart.getChild(SHELL);
+        shell.scale(new Vec3f(-0.01f, -0.01f, -0.01f));
         midOrb = modelPart.getChild(MID_ORB);
         innerOrb = modelPart.getChild(INNER_ORB);
     }
@@ -33,30 +39,38 @@ public class CreativeGeneratorBlockEntityRenderer implements BlockEntityRenderer
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
         modelPartData.addChild(
+                FRAME,
+                ModelPartBuilder
+                        .create()
+                        .uv(64, 0)
+                        .cuboid(-8f, -8f, -8f, 16.0f, 16.0f, 16.0f),
+                ModelTransform.pivot(8.0f, 8.0f, 8.0f)
+        );
+        modelPartData.addChild(
                 SHELL,
                 ModelPartBuilder
                         .create()
                         .uv(0, 0)
-                        .cuboid(0.0f, 0.0f, 0.0f, 16.0f, 16.0f, 16.0f),
-                ModelTransform.NONE
+                        .cuboid(-8f, -8f, -8f, 16.0f, 16.0f, 16.0f),
+                ModelTransform.pivot(8.0f, 8.0f, 8.0f)
         );
         modelPartData.addChild(
                 MID_ORB,
                 ModelPartBuilder
                         .create()
                         .uv(0, 34)
-                        .cuboid(0f, 0f, 0f, 8.0f, 8.0f, 8.0f),
-                ModelTransform.pivot(16.0f, 16.0f, 16.0f)
+                        .cuboid(-4f, -4f, -4f, 8.0f, 8.0f, 8.0f),
+                ModelTransform.pivot(8.0f, 8.0f, 8.0f)
         );
         modelPartData.addChild(
                 INNER_ORB,
                 ModelPartBuilder
                         .create()
                         .uv(0, 56)
-                        .cuboid(0f, 0f, 0f, 4.0f, 4.0f, 4.0f),
-                ModelTransform.pivot(16f, 16f, 16f)
+                        .cuboid(-2f, -2f, -2f, 4.0f, 4.0f, 4.0f),
+                ModelTransform.pivot(8.0f, 8.0f, 8.0f)
         );
-        return TexturedModelData.of(modelData, 64, 64);
+        return TexturedModelData.of(modelData, 128, 64);
     }
 
     @Override
@@ -64,15 +78,13 @@ public class CreativeGeneratorBlockEntityRenderer implements BlockEntityRenderer
         matrices.push();
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(TEXTURE));
         shell.render(matrices, vertexConsumer, 15728880, overlay);
+        frame.render(matrices, vertexConsumer, light, overlay);
         long time = entity.getWorld().getTime();
-        final long cycle = 24;
-        matrices.push();
-        float scale = 0.1f*(float)Math.sin((double)time/cycle*2*Math.PI);
-        //matrices.scale(1.0f+scale, 1.0f+scale, 1.0f+scale);
+        float scale = 0.1f*(float)Math.sin((double)time/CYCLE*2*Math.PI);
+        midOrb.xScale = midOrb.yScale = midOrb.zScale = 1.0f + scale;
+	    scale *= 0.5f;
+        innerOrb.xScale = innerOrb.yScale = innerOrb.zScale = 1.0f + scale;
         midOrb.render(matrices, vertexConsumer, 15728880, overlay);
-        matrices.pop();
-        scale *= 0.5f;
-        //matrices.scale(1.0f+scale, 1.0f+scale, 1.0f+scale);
         innerOrb.render(matrices, vertexConsumer, 15728880, overlay);
         matrices.pop();
     }
