796a80a02be688aee7bf6aced23f5698d75e3c22
fishBone000 <FishBoneEK@outlook.com>
2023-01-16 23:07:16 +0800

    Wind Turbine WIP

--- src/main/java/com/brainsmash/broken_world/blocks/electric/generator/WindTurbineBlock.java
+++ src/main/java/com/brainsmash/broken_world/blocks/electric/generator/WindTurbineBlock.java
@@ -1,7 +1,7 @@
 package com.brainsmash.broken_world.blocks.electric.generator;
 
 import com.brainsmash.broken_world.blocks.electric.base.PowerBlock;
-import com.brainsmash.broken_world.blocks.entity.electric.generator.HydroGeneratorEntity;
+import com.brainsmash.broken_world.blocks.entity.electric.generator.WindTurbineEntity;
 import com.brainsmash.broken_world.registry.PointOfInterestRegister;
 import com.mojang.logging.LogUtils;
 import net.minecraft.block.Block;
@@ -9,6 +9,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
@@ -19,7 +20,6 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.poi.PointOfInterest;
 import net.minecraft.world.poi.PointOfInterestStorage;
 import org.jetbrains.annotations.Nullable;
@@ -39,29 +39,81 @@ public class WindTurbineBlock extends PowerBlock {
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new HydroGeneratorEntity(pos,state);
+        return new WindTurbineEntity(pos,state);
     }
 
     private List<BlockPos> getNearbyWindTurbines(ServerWorld world, BlockPos pos){
         PointOfInterestStorage pointOfInterestStorage = world.getPointOfInterestStorage();
         Stream<PointOfInterest> stream = pointOfInterestStorage.getInCircle(
-                poiType -> {
-                    LOGGER.info(poiType.getKey().toString());
-                    return poiType.matchesId(PointOfInterestRegister.WIND_TURBINE);
-                },
+                poiType -> poiType.matchesId(PointOfInterestRegister.WIND_TURBINE),
                 pos,
                 20,
                 PointOfInterestStorage.OccupationStatus.ANY
-        );
+        ).filter(poi -> !poi.getPos().equals(pos));
         return stream.map(PointOfInterest::getPos).collect(Collectors.toList());
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        super.onStateReplaced(state, world, pos, newState, moved);
+        if(world.isClient || state.isOf(newState.getBlock()))
+            return;
+
+        List<BlockPos> list = getNearbyWindTurbines((ServerWorld) world, pos);
+        for(BlockPos blockPos : list){
+            BlockEntity entity = world.getBlockEntity(blockPos);
+            if(!(entity instanceof WindTurbineEntity))
+                continue;
+            ((WindTurbineEntity) entity).lessCrowded();
+            BlockState blockState = world.getBlockState(blockPos).with(Properties.LIT, ((WindTurbineEntity) entity).isRunning());
+            world.setBlockState(blockPos, blockState);
+        }
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        super.onPlaced(world, pos, state, placer, itemStack);
+        if(world.isClient)
+            return;
+        LOGGER.info("Wind Turbine onPlace called. ");
+        List<BlockPos> list = getNearbyWindTurbines((ServerWorld) world, pos);
+        for(BlockPos blockPos : list){
+            BlockEntity entity = world.getBlockEntity(blockPos);
+            if(!(entity instanceof WindTurbineEntity))
+                continue;
+            ((WindTurbineEntity) entity).moreCrowded();
+            BlockState blockState = world.getBlockState(blockPos).with(Properties.LIT, ((WindTurbineEntity)entity).isRunning());
+            world.setBlockState(blockPos, blockState);
+        }
+        BlockEntity entity = world.getBlockEntity(pos);
+        if(entity instanceof WindTurbineEntity && !list.isEmpty()){
+            ((WindTurbineEntity) entity).moreCrowded();
+        }
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if(!world.isClient) {
-            LOGGER.info("Nearby wind turbines: " + getNearbyWindTurbines((ServerWorld) world, pos).toString());
+            BlockEntity entity = world.getBlockEntity(pos);
+            LOGGER.info("Running: " + (!(entity instanceof WindTurbineEntity) ?
+                                      "BlockEntity not instance of WindTurbineEntity. " :
+                                      ((WindTurbineEntity) entity).isRunning())
+            );
+            List<BlockPos> list = getNearbyWindTurbines((ServerWorld) world, pos);
+            LOGGER.info("Nearby wind turbines: ");
+            for(BlockPos blockPos : list){
+                LOGGER.info(blockPos.toString() + ", distance: " + pos.getSquaredDistance(blockPos));
+            }
         }
         return super.onUse(state, world, pos, player, hand, hit);
+    }
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockEntity entity = ctx.getWorld().getBlockEntity(ctx.getBlockPos());
+        return super.getPlacementState(ctx)
+                .with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite())
+                .with(Properties.LIT, entity instanceof WindTurbineEntity && ((WindTurbineEntity) entity).isRunning());
+        // I know it won't work, but PJ says he will handle it
     }
 
     @Override
--- src/main/java/com/brainsmash/broken_world/blocks/entity/electric/generator/WindTurbineEntity.java
+++ src/main/java/com/brainsmash/broken_world/blocks/entity/electric/generator/WindTurbineEntity.java
@@ -1,23 +1,34 @@
 package com.brainsmash.broken_world.blocks.entity.electric.generator;
 
-import com.brainsmash.broken_world.blocks.electric.generator.WindTurbineBlock;
-import com.brainsmash.broken_world.blocks.entity.electric.base.EnergyManager;
 import com.brainsmash.broken_world.blocks.entity.electric.base.PowerBlockEntity;
 import com.brainsmash.broken_world.registry.BlockRegister;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import org.jetbrains.annotations.Nullable;
 
 public class WindTurbineEntity extends PowerBlockEntity {
+
+    protected int nearbyTurbineCount = 0;
+    protected final boolean bellowSeaLvl;
     public WindTurbineEntity(BlockPos pos, BlockState state) {
         super(BlockRegister.WIND_TURBINE_ENTITY_TYPE, pos, state);
         setMaxCapacity(2000);
-        setGenerate(0);
+        bellowSeaLvl = pos.getY() < this.pos
+        setGenerate(calculateGenerate());
+        running = !bellowSeaLvl;
     }
 
+    protected int calculateGenerate(){
+        return 114514;
+    }
+
+    public void moreCrowded(){
+        running = false;
+        nearbyTurbineCount++;
+    }
+
+    public void lessCrowded(){
+        nearbyTurbineCount--;
+        if(nearbyTurbineCount == 0)
+            running = !bellowSeaLvl;
+    }
 }
