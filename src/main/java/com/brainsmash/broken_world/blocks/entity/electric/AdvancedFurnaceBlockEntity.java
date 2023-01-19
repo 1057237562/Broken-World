b84b1b4a4fc36ab81185cb5b90ecb03966d0401f
PANJIAN <1057237562@qq.com>
2023-01-19 18:56:23 +0800

    Update

--- src/main/java/com/brainsmash/broken_world/blocks/entity/electric/AdvancedFurnaceBlockEntity.java
+++ src/main/java/com/brainsmash/broken_world/blocks/entity/electric/AdvancedFurnaceBlockEntity.java
@@ -31,6 +31,7 @@ import java.util.Random;
 public class AdvancedFurnaceBlockEntity extends ConsumerBlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(23, ItemStack.EMPTY);
     public final Random random = new Random();
+    private Item lastItem;
 
     public AdvancedFurnaceBlockEntity(BlockPos pos, BlockState state) {
         super(BlockRegister.ADVANCED_FURNACE_ENTITY_TYPE,pos, state);
@@ -86,6 +87,10 @@ public class AdvancedFurnaceBlockEntity extends ConsumerBlockEntity implements N
                     inventory.get(0).decrement(1);
                     progression = 0;
                 }
+                if(!inventory.get(0).getItem().equals(lastItem)){
+                    lastItem = inventory.get(0).getItem();
+                    progression = 0;
+                }
             }else{
                 running = false;
                 progression = 0;
@@ -105,6 +110,7 @@ public class AdvancedFurnaceBlockEntity extends ConsumerBlockEntity implements N
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
         Inventories.readNbt(nbt, this.inventory);
+        lastItem = inventory.get(0).getItem();
     }
 
     @Override
