39b17de65be4d8d977667660bc4750871844f33d
Authenticator <1057237562@outlook.com>
2023-06-07 17:15:03 +0800

    altered

--- src/main/java/com/brainsmash/broken_world/datagen/ModelGenerator.java
+++ src/main/java/com/brainsmash/broken_world/datagen/ModelGenerator.java
@@ -5,6 +5,7 @@ import com.brainsmash.broken_world.blocks.electric.base.BatteryBlock;
 import com.brainsmash.broken_world.blocks.electric.base.CableBlock;
 import com.brainsmash.broken_world.blocks.electric.base.ConsumerBlock;
 import com.brainsmash.broken_world.blocks.electric.base.PowerBlock;
+import com.brainsmash.broken_world.items.weapons.guns.GunItem;
 import com.brainsmash.broken_world.registry.BlockRegister;
 import com.brainsmash.broken_world.registry.ItemRegister;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -23,7 +24,7 @@ public class ModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        for (int i = 0; i < BlockRegister.blocks.length; i++) {
+        for (int i = 57; i < BlockRegister.blocks.length; i++) {
             if (BlockRegister.blocks[i].getStateManager().getProperties().isEmpty() && !(BlockRegister.blocks[i] instanceof BatteryBlock) && !(BlockRegister.blocks[i] instanceof PowerBlock) && !(BlockRegister.blocks[i] instanceof ConsumerBlock) && !(BlockRegister.blocks[i] instanceof CableBlock)) {
                 blockStateModelGenerator.registerSimpleCubeAll(BlockRegister.blocks[i]);
             }
@@ -33,12 +34,14 @@ public class ModelGenerator extends FabricModelProvider {
     @Override
     public void generateItemModels(net.minecraft.data.client.ItemModelGenerator itemModelGenerator) {
         for (int i = 0; i < ItemRegister.items.length; i++) {
-            itemModelGenerator.register(ItemRegister.items[i], Models.GENERATED);
+            if (!(ItemRegister.items[i] instanceof GunItem)) {
+                itemModelGenerator.register(ItemRegister.items[i], Models.GENERATED);
+            }
         }
         for (int i = 0; i < ItemRegister.bucket_item.length; i++) {
             itemModelGenerator.register(ItemRegister.bucket_item[i], Models.GENERATED);
         }
-        for (int i = 0; i < BlockRegister.blocks.length; i++) {
+        for (int i = 57; i < BlockRegister.blocks.length; i++) {
             itemModelGenerator.register(BlockRegister.blockitems[i],
                     new Model(Optional.of(new Identifier(Main.MODID, "block/" + BlockRegister.blocknames[i])),
                             Optional.empty()));
