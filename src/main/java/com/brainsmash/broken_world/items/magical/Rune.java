fe42c3053822417ef164039621c50f65ad64e0ef
Authenticator <1057237562@outlook.com>
2023-06-19 11:06:39 +0800

    proceed wand screen handler

---
+++ src/main/java/com/brainsmash/broken_world/items/magical/Rune.java
@@ -0,0 +1,10 @@
+package com.brainsmash.broken_world.items.magical;
+
+import net.minecraft.item.Item;
+
+public class Rune extends Item {
+    public Rune(Settings settings) {
+        super(settings);
+    }
+    
+}
--- src/main/java/com/brainsmash/broken_world/items/magical/Wand.java
+++ src/main/java/com/brainsmash/broken_world/items/magical/Wand.java
@@ -1,27 +1,41 @@
 package com.brainsmash.broken_world.items.magical;
 
+import com.brainsmash.broken_world.screenhandlers.WandScreenHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class Wand extends Item {
-    public Wand(Settings settings) {
+
+    public int runeSize = 9;
+    public static Text CONTAINER_NAME = Text.translatable("container.wand");
+
+    public Wand(Settings settings, int size) {
         super(settings);
+        runeSize = size;
     }
 
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 
-        NbtCompound nbtCompound = user.getStackInHand(hand).getNbt();
-        if (user.isSneaking()) {
-
+        if (user.isSneaking() && hand == Hand.MAIN_HAND) {
+            user.openHandledScreen(new SimpleNamedScreenHandlerFactory(
+                    (syncId, inventory, player) -> new WandScreenHandler(ScreenHandlerType.GENERIC_9X1, syncId,
+                            inventory, 1), CONTAINER_NAME));
         } else {
+            NbtCompound nbtCompound = user.getStackInHand(hand).getNbt();
+            for (NbtElement element : nbtCompound.getList("inventory", NbtElement.COMPOUND_TYPE)) {
 
+            }
         }
         return super.use(world, user, hand);
     }
