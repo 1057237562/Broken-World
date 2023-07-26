e35553e5bb0c32ff71549ce061f95b5516ce6a33
Authenticator <1057237562@outlook.com>
2023-07-26 16:16:20 +0800

    rename bad naming

--- src/main/java/com/brainsmash/broken_world/registry/BlockRegister.java
+++ src/main/java/com/brainsmash/broken_world/registry/BlockRegister.java
@@ -22,7 +22,7 @@ import com.brainsmash.broken_world.blocks.gen.RubberSaplingGenerator;
 import com.brainsmash.broken_world.blocks.magical.InfusedCrystalBlock;
 import com.brainsmash.broken_world.blocks.model.BottomTopBlock;
 import com.brainsmash.broken_world.blocks.model.TeleporterFrameBlock;
-import com.brainsmash.broken_world.blocks.multipblock.ColliderMultiBlock;
+import com.brainsmash.broken_world.blocks.multiblock.ColliderMultiBlock;
 import com.brainsmash.broken_world.blocks.ores.MagnetiteBlock;
 import com.brainsmash.broken_world.registry.enums.BlockRegistry;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
