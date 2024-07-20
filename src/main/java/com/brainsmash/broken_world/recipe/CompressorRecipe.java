9ecc50d13449aa1ab4c706225934ca2135f3fc6b
Authenticator <1057237562@outlook.com>
2024-07-20 11:14:11 +0800

    add recipe for compressed bone block
    Signed-off-by: Authenticator <1057237562@outlook.com>

--- src/main/java/com/brainsmash/broken_world/recipe/CompressorRecipe.java
+++ src/main/java/com/brainsmash/broken_world/recipe/CompressorRecipe.java
@@ -1,6 +1,8 @@
 package com.brainsmash.broken_world.recipe;
 
+import com.brainsmash.broken_world.registry.BlockRegister;
 import com.brainsmash.broken_world.registry.ItemRegister;
+import com.brainsmash.broken_world.registry.enums.BlockRegistry;
 import com.brainsmash.broken_world.registry.enums.ItemRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -18,7 +20,9 @@ public class CompressorRecipe {
                 new ItemStack(ItemRegister.items[ItemRegistry.IRON_PLATE.ordinal()], 2));
         registCompressorRecipe(ItemRegister.items[ItemRegistry.STEEL_INGOT.ordinal()].getDefaultStack(),
                 new ItemStack(ItemRegister.items[ItemRegistry.STEEL_PLATE.ordinal()], 1));
-        registCompressorRecipe(new ItemStack(Items.STONE, 4), new ItemStack(Items.COBBLED_DEEPSLATE));
+        registCompressorRecipe(new ItemStack(Items.STONE, 9), new ItemStack(Items.COBBLED_DEEPSLATE));
+        registCompressorRecipe(new ItemStack(Items.BONE_BLOCK, 9),
+                new ItemStack(BlockRegister.blockitems[BlockRegistry.COMPRESSED_BONE_BLOCK.ordinal()]));
     }
 
     public static void registCompressorRecipe(ItemStack a, ItemStack b) {
