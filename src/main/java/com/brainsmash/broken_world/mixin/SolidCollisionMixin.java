d0c43c9a056e9d725ccf77976fae1c5dca760820
Authenticator <1057237562@outlook.com>
2024-07-20 11:41:11 +0800

    add loot table for crucible
    Signed-off-by: Authenticator <1057237562@outlook.com>

--- src/main/java/com/brainsmash/broken_world/mixin/SolidCollisionMixin.java
+++ src/main/java/com/brainsmash/broken_world/mixin/SolidCollisionMixin.java
@@ -3,15 +3,13 @@ package com.brainsmash.broken_world.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(LivingEntity.class)
+@Mixin(PlayerEntity.class)
 public abstract class SolidCollisionMixin extends Entity {
-    @Shadow
-    public abstract boolean isAlive();
 
     public SolidCollisionMixin(EntityType<?> type, World world) {
         super(type, world);
