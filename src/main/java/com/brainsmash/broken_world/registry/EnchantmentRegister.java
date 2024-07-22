d0f9e519247a2b317570a22c3107080b47390a4a
fishBone000 <FishBoneEK@outlook.com>
2024-07-22 17:11:02 +0000

    Add Soul Pully Enchantment

--- src/main/java/com/brainsmash/broken_world/registry/EnchantmentRegister.java
+++ src/main/java/com/brainsmash/broken_world/registry/EnchantmentRegister.java
@@ -1,6 +1,7 @@
 package com.brainsmash.broken_world.registry;
 
 import com.brainsmash.broken_world.enchantment.SoulLeechingEnchantment;
+import com.brainsmash.broken_world.enchantment.SoulPullyEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -9,8 +10,10 @@ import static com.brainsmash.broken_world.Main.MODID;
 
 public class EnchantmentRegister {
     public static Enchantment SOUL_LEECHING = new SoulLeechingEnchantment();
+    public static Enchantment SOUL_PULLY = new SoulPullyEnchantment();
 
     public static void registerEnchantments() {
         Registry.register(Registry.ENCHANTMENT, new Identifier(MODID, "soul_leeching"), SOUL_LEECHING);
+        Registry.register(Registry.ENCHANTMENT, new Identifier(MODID, "soul_pully"), SOUL_PULLY);
     }
 }
