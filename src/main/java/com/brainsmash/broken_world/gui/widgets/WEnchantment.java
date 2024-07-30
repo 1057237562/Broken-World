265b35b51199d9e6ae175030205297f9af2b2ef8
fishBone000 <FishBoneEK@outlook.com>
2024-07-30 13:16:09 +0000

    Fix WEnchantment UV problem

--- src/main/java/com/brainsmash/broken_world/gui/widgets/WEnchantment.java
+++ src/main/java/com/brainsmash/broken_world/gui/widgets/WEnchantment.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.TooltipBuilder;
 import io.github.cottonmc.cotton.gui.widget.WWidget;
+import io.github.cottonmc.cotton.gui.widget.data.InputResult;
 import io.github.cottonmc.cotton.gui.widget.data.Texture;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
@@ -27,15 +28,15 @@ public class WEnchantment extends WWidget {
     public static final Identifier ORB_AND_NUMBERS = new Identifier(Main.MODID, "textures/gui/infusion_table/orb_and_numbers.png");
     public static final int WIDTH = 108;
     public static final int HEIGHT = 19;
-    public static final Texture BACKGROUND_AVAILABLE = new Texture(TEXTURE, 0, 166, WIDTH, 166 + HEIGHT);
-    public static final Texture BACKGROUND_UNAVAILABLE = new Texture(TEXTURE, 0, 185, WIDTH, 185 + HEIGHT);
-    public static final Texture BACKGROUND_MOUSE_HOVER = new Texture(TEXTURE, 0, 204, WIDTH, 204 + HEIGHT);
-    public static final Texture ORB_BRIGHT_SMALL = new Texture(ORB_AND_NUMBERS, 0, 0, 16, 16);
-    public static final Texture ORB_BRIGHT_MEDIUM = new Texture(ORB_AND_NUMBERS, 16, 0, 32, 16);
-    public static final Texture ORB_BRIGHT_LARGE = new Texture(ORB_AND_NUMBERS, 32, 0, 48, 16);
-    public static final Texture ORB_DIM_SMALL = new Texture(ORB_AND_NUMBERS, 0, 0, 16, 16);
-    public static final Texture ORB_DIM_MEDIUM = new Texture(ORB_AND_NUMBERS, 0, 0, 16, 16);
-    public static final Texture ORB_DIM_LARGE = new Texture(ORB_AND_NUMBERS, 0, 0, 16, 16);
+    public static final Texture BACKGROUND_AVAILABLE = new Texture(TEXTURE, 0, 166 / 256.0f, WIDTH / 256.0f, (166 + HEIGHT) / 256.0f);
+    public static final Texture BACKGROUND_UNAVAILABLE = new Texture(TEXTURE, 0, 185 / 256.0f, WIDTH / 256.0f, (185 + HEIGHT) / 256.0f);
+    public static final Texture BACKGROUND_MOUSE_HOVER = new Texture(TEXTURE, 0, 204 / 256.0f, WIDTH / 256.0f, (204 + HEIGHT) / 256.0f);
+    public static final Texture ORB_BRIGHT_SMALL = new Texture(ORB_AND_NUMBERS, 0, 0, 16 / 128.0f, 16 / 128.0f);
+    public static final Texture ORB_BRIGHT_MEDIUM = new Texture(ORB_AND_NUMBERS, 16 / 128.0f, 0, 32 / 128.0f, 16 / 128.0f);
+    public static final Texture ORB_BRIGHT_LARGE = new Texture(ORB_AND_NUMBERS, 32 / 128.0f, 0, 48 / 128.0f, 16 / 128.0f);
+    public static final Texture ORB_DIM_SMALL = new Texture(ORB_AND_NUMBERS, 0, 0, 16 / 128.0f, 16 / 128.0f);
+    public static final Texture ORB_DIM_MEDIUM = new Texture(ORB_AND_NUMBERS, 0, 0, 16 / 128.0f, 16 / 128.0f);
+    public static final Texture ORB_DIM_LARGE = new Texture(ORB_AND_NUMBERS, 0, 0, 16 / 128.0f, 16 / 128.0f);
 
 
     public Enchantment enchantment = null;
@@ -52,8 +53,8 @@ public class WEnchantment extends WWidget {
         numberForegrounds = new Texture[10];
         numberBackgrounds = new Texture[10];
         for (int i = 0; i < 10; i++) {
-            numberForegrounds[i] = new Texture(ORB_AND_NUMBERS, i * 8, 32, i * 8 + 8, 41);
-            numberBackgrounds[i] = new Texture(ORB_AND_NUMBERS, i * 8, 41, i * 8 + 8, 50);
+            numberForegrounds[i] = new Texture(ORB_AND_NUMBERS, (i * 8) / 128.0f, 32 / 128.0f, (i * 8 + 8) / 128.0f, 41 / 128.0f);
+            numberBackgrounds[i] = new Texture(ORB_AND_NUMBERS, (i * 8) / 128.0f, 41 / 128.0f, (i * 8 + 8) / 128.0f, 50 / 128.0f);
         }
     }
 
@@ -87,16 +88,15 @@ public class WEnchantment extends WWidget {
 //        int n = ((EnchantmentScreenHandler)this.handler).getLapisCount();
 
 //        int widgetX = i + 60;
-        int widgetX = x;
-        int widgetY = y;
-        int enchantmentPhrasesX = widgetX + 20;
+        int phrasesX = x + 20;
+        int phrasesY = y + 2;
 //        this.setZOffset(0);
 //        RenderSystem.setShader(GameRenderer::getPositionTexShader);
 //        RenderSystem.setShaderTexture(0, TEXTURE);
 //        int power = ((EnchantmentScreenHandler)this.handler).enchantmentPower[o];
 //        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         if (enchantment == null) {
-            ScreenDrawing.texturedRect(matrices, widgetX, widgetY, WIDTH, HEIGHT, BACKGROUND_UNAVAILABLE, 0xFFFFFFFF);
+            ScreenDrawing.texturedRect(matrices, x, y, WIDTH, HEIGHT, BACKGROUND_UNAVAILABLE, 0xFFFFFFFF);
             return;
         }
         String powerString = "" + enchantmentPower;
@@ -104,28 +104,31 @@ public class WEnchantment extends WWidget {
         StringVisitable stringVisitable = EnchantingPhrases.getInstance().generatePhrase(this.textRenderer, phrasesWidthLimit);
         int t = 6839882;
         if (!available) {
-            ScreenDrawing.texturedRect(matrices, widgetX, widgetY, WIDTH, HEIGHT, BACKGROUND_UNAVAILABLE, 0xFFFFFFFF);
+            ScreenDrawing.texturedRect(matrices, x, y, WIDTH, HEIGHT, BACKGROUND_UNAVAILABLE, 0xFFFFFFFF);
             // draw exp orbs
             drawOrbAndLevelNumbers(matrices, enchantmentPower, available);
 //            this.drawTexture(matrices, widgetX + 1, j + 15 + 19 * o, 16 * o, 239, 16, 16);
-//            this.textRenderer.drawTrimmed(stringVisitable, enchantmentPhrasesX, 2, phrasesWidthLimit, (t & 0xFEFEFE) >> 1);
-            ScreenDrawing.drawString(matrices, stringVisitable.getString(), enchantmentPhrasesX, 2, (t & 0xFEFEFE) >> 1);
+            this.textRenderer.drawTrimmed(stringVisitable, phrasesX, phrasesY, phrasesWidthLimit, (t & 0xFEFEFE) >> 1);
+//            ScreenDrawing.drawString(matrices, stringVisitable.getString(), phrasesX, 2, 0xFF_000000 + (t & 0xFEFEFE) >> 1);
             t = 4226832;
         } else {
             if (mouseX >= 0 && mouseY >= 0 && mouseX < WIDTH && mouseY < HEIGHT) {
-                ScreenDrawing.texturedRect(matrices, widgetX, widgetY, WIDTH, HEIGHT, BACKGROUND_MOUSE_HOVER, 0xFFFFFFFF);
+                ScreenDrawing.texturedRect(matrices, x, y, WIDTH, HEIGHT, BACKGROUND_MOUSE_HOVER, 0xFFFFFFFF);
                 t = 0xFFFF80;
             } else {
-                ScreenDrawing.texturedRect(matrices, widgetX, widgetY, WIDTH, HEIGHT, BACKGROUND_AVAILABLE, 0xFFFFFFFF);
+                ScreenDrawing.texturedRect(matrices, x, y, WIDTH, HEIGHT, BACKGROUND_AVAILABLE, 0xFFFFFFFF);
             }
             // draw exp orbs
             drawOrbAndLevelNumbers(matrices, enchantmentPower, available);
 //            this.drawTexture(matrices, widgetX + 1, j + 15 + 19 * o, 16 * o, 223, 16, 16);
-//            this.textRenderer.drawTrimmed(stringVisitable, enchantmentPhrasesX, 2, phrasesWidthLimit, t);
-            ScreenDrawing.drawString(matrices, stringVisitable.getString(), enchantmentPhrasesX, 2, t);
+            this.textRenderer.drawTrimmed(stringVisitable, phrasesX, phrasesY, phrasesWidthLimit, t);
+//            ScreenDrawing.drawString(matrices, stringVisitable.getString(), phrasesX, 2, 0xFF_000000 + t);
             t = 8453920;
         }
-//        this.textRenderer.drawWithShadow(matrices, powerString, (float)(enchantmentPhrasesX + 86 - this.textRenderer.getWidth(powerString)), 9.0f, t);
+//        this.textRenderer.drawWithShadow(matrices, powerString, (float)(phrasesX + 86 - this.textRenderer.getWidth(powerString)), 9.0f, t);
+        if (0 <= mouseX && mouseX <= WIDTH && 0 <= mouseY && mouseY <= HEIGHT) {
+            renderTooltip(matrices, x, y, mouseX, mouseY);
+        }
 //         */
     }
 
