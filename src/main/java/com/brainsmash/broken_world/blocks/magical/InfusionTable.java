ea22e4550a7a8690d77a979e06092c0bb920ff5a
Authenticator <1057237562@outlook.com>
2024-08-01 23:34:25 +0800

    set collision shape
    Signed-off-by: Authenticator <1057237562@outlook.com>

--- src/main/java/com/brainsmash/broken_world/blocks/magical/InfusionTable.java
+++ src/main/java/com/brainsmash/broken_world/blocks/magical/InfusionTable.java
@@ -5,12 +5,16 @@ import com.brainsmash.broken_world.blocks.model.CustomModelBlock;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -34,5 +38,15 @@ public class InfusionTable extends BlockWithEntity implements CustomModelBlock {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0, 0, 0, 1, 0.7, 1);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0, 0, 0, 1, 0.7, 1);
     }
 }
