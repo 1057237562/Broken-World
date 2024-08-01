5963016c2aa67dd613a2c686d6a64fe9d0f5f005
Authenticator <1057237562@outlook.com>
2024-08-01 23:27:58 +0800

    add texture
    Signed-off-by: Authenticator <1057237562@outlook.com>

--- src/main/java/com/brainsmash/broken_world/blocks/magical/InfusionTable.java
+++ src/main/java/com/brainsmash/broken_world/blocks/magical/InfusionTable.java
@@ -2,7 +2,7 @@ package com.brainsmash.broken_world.blocks.magical;
 
 import com.brainsmash.broken_world.blocks.entity.magical.InfusionTableEntity;
 import com.brainsmash.broken_world.blocks.model.CustomModelBlock;
-import com.brainsmash.broken_world.screens.cotton.InfusionTableScreen;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
@@ -29,5 +29,10 @@ public class InfusionTable extends BlockWithEntity implements CustomModelBlock {
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
     }
 }
